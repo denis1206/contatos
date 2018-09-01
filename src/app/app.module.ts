edb07ef04a150b1ae64fc2fdb1d74818a0532f65
Denis Silva <denis1206@hotmail.com>
2018-09-01 10:48:13 -0300

    aula 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ContactProvider } from '../providers/contact/contact';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBwn999zdmTwyPcioG6GltV6oTnHNXqmuk",
+      authDomain: "contatos-25a59.firebaseapp.com",
+      databaseURL: "https://contatos-25a59.firebaseio.com",
+      projectId: "contatos-25a59",
+      storageBucket: "contatos-25a59.appspot.com",
+      messagingSenderId: "160533270775"
+    }),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
+  ]
+})
+export class AppModule {}
